2a08244ed354516d01d16b813b49b7e66ea8426d
UYoung <wawa634@gmail.com>
2022-05-30 12:22:17 +0900

    commit2
    commit2

---
+++ todolist/lib/part_C/UY/list_item.dart
@@ -0,0 +1,11 @@
+class ListItem {
+  String code;
+  String content;
+  String check;
+
+  ListItem({
+    required this.code,
+    required this.content,
+    required this.check,
+  });
+}
--- todolist/lib/part_C/UY/uy.dart
+++ todolist/lib/part_C/UY/uy.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:todolist/part_C/UY/list_item.dart';
 
 class ListPage extends StatefulWidget {
   const ListPage({Key? key}) : super(key: key);
@@ -10,6 +14,16 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  late List todolist;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    todolist = [];
+    getJSONData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +34,27 @@ class _ListPageState extends State<ListPage> {
       ),
       body: Center(
         child: Column(
-          children: [],
+          children: [
+            ListView.builder(
+                // list가 있는만큼 만들어주는애
+
+                itemCount: todolist.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      //actionKind(context, position);
+                    },
+                    child: Card(
+                      child: Row(
+                        children: [
+                          Text(todolist[index]['check']),
+                          Text(todolist[index]['content']),
+                        ],
+                      ),
+                    ),
+                  );
+                })
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -32,4 +66,34 @@ class _ListPageState extends State<ListPage> {
       ),
     );
   }
-}
+
+  Future<bool> getJSONData() async {
+    //비동기 함수 == 작업하면서 화면구성도 같이하겠다!
+    //이럴땐 (주소) var를 많이 씀.
+    todolist.clear();
+    var url =
+        Uri.parse("http://localhost:8080/Flutter/todolist_select_all.jsp");
+    //http 가 위주소를 다가져옴 //await <- 빌드가 끝날때까지 일단 기다려
+    var response = await http.get(url);
+
+    /* 내가 알아보기 쉽게 변형해주기! */
+    setState(() {
+      //화면 구성이 바뀌니까 setState 사용!
+      //body는 통째로 보여줘
+      //한글해주는건 utf8.decode(response.bodyBytes)로 해야함!
+      var dataConvertedJSON = json.decode(utf8.decode(response.bodyBytes));
+      List result = dataConvertedJSON['results']; //results 키값!
+
+      //print(result);
+      //result[0]['code'] = S001 <- 리스트에 넣어준거 불러오는법
+
+      //불러오고 수정한값을 이제 넣어주자!
+      todolist.addAll(result);
+      //print(data[0]['code']); //flutter: S001
+      print(todolist[1]);
+    });
+
+    //print(response.body);
+    return true;
+  }
+}//end
