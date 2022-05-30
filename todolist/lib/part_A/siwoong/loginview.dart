1b806d12b4d34398ca2f721545744ae0370276d7
siwoong <swyoo39@naver.com>
2022-05-30 14:45:05 +0900

    0530_loginView

--- todolist/lib/part_A/siwoong/loginview.dart
+++ todolist/lib/part_A/siwoong/loginview.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,12 +14,20 @@ class _LoginViewState extends State<LoginView> {
   // Property
   late TextEditingController uId;
   late TextEditingController uPw;
+  late String userID;
+  late String userPW;
+
+  late List data;
 
   @override
   void initState() {
     super.initState();
     uId = TextEditingController();
     uPw = TextEditingController();
+    userID = '';
+    userPW = '';
+
+    data = [];
   }
 
   @override
@@ -77,12 +87,15 @@ class _LoginViewState extends State<LoginView> {
                         emptyID(context);
                       } else if (uPw.text.trim().isEmpty) {
                         emptyPW(context);
-                      } else if (uId.text.trim() != 'root') {
-                        wrongID(context);
-                      } else if (uPw.text.trim() != 'qwer1234') {
-                        wrongPW(context);
                       } else {
-                        logInSuccess(context);
+                        setState(() {
+                          userID = uId.text.trim();
+                          userPW = uPw.text.trim();
+                        });
+                        getJSONData();
+                        // data 오류
+                        print(data);
+                        logInCheck(context);
                       }
                     },
                     child: const Text('Log in'),
@@ -101,7 +114,19 @@ class _LoginViewState extends State<LoginView> {
                         child: const Text('회원가입 하기'),
                       ),
                     ],
-                  )
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('아직 회원이 아니신가요?'),
+                      TextButton(
+                        onPressed: () {
+                          // Navigator.pushNamed(context, '/signin');
+                        },
+                        child: const Text('회원가입 하기'),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
@@ -132,47 +157,66 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 
-  wrongID(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('등록된 ID를 입력하세요.'),
-        duration: Duration(seconds: 1),
-        backgroundColor: Colors.red,
-      ),
-    );
+  Future<bool> getJSONData() async {
+    // 비동기 방식 async : 동시에 실행되고
+    var url = Uri.parse(
+        'http://localhost:8080/flutter/todolist_semi/todolist_user_select.jsp?uId=$userID&uPw=$userPW');
+    var response = await http.get(url);
+    // await, build가 data를 기다림
+    // get 방식
+    var dataConvertedJSON = json.decode(utf8.decode(response.bodyBytes));
+    // body 자체로는 decode하지 못한다 : bodyBytes
+    List result = dataConvertedJSON['results'];
+    // 행렬의 형태로 result에 저장한다. [2,4]
+
+    setState(() {
+      data = []; // 초기화 안하면 계속 누적되어서 출력된다.
+      data.addAll(result);
+    });
+    print(data);
+    return true;
   }
 
-  wrongPW(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('PW를 확인해주세요.'),
-        duration: Duration(seconds: 1),
-        backgroundColor: Colors.amber,
-      ),
-    );
-  }
-
-  logInSuccess(BuildContext context) {
+  logInCheck(BuildContext context) {
     showDialog(
         context: context,
         builder: (BuildContext ctx) {
-          return AlertDialog(
-            title: const Text(
-              'Welcome!',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+          if (data.isEmpty) {
+            return AlertDialog(
+              title: const Text(
+                '로그인 실패!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            content: const Text('Log-in complete'),
-            actions: [
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    // Navigator.pushNamed(context, '/list');
-                  },
-                  child: const Text('OK'))
-            ],
-          );
+              content: const Text('ID와 PW를 확인해주세요.'),
+              actions: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('OK'))
+              ],
+            );
+          } else {
+            return AlertDialog(
+              title: const Text(
+                '로그인 성공!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              content: const Text('로그인에 성공하였습니다.'),
+              actions: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      // Navigator.push(context, MaterialPageRoute(builder: (context)=>));
+                    },
+                    child: const Text('OK'))
+              ],
+            );
+          }
         });
   }
 }
