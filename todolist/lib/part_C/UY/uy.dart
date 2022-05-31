6635495418fc8224c6f5a83034cfb1b5304873bb
UYoung <wawa634@gmail.com>
2022-05-31 10:44:18 +0900

    주석제거
    주석제거

--- todolist/lib/part_C/UY/uy.dart
+++ todolist/lib/part_C/UY/uy.dart
@@ -189,31 +189,19 @@ class _ListPageState extends State<ListPage>
   }
 
   Future<bool> getJSONData() async {
-    //비동기 함수 == 작업하면서 화면구성도 같이하겠다!
-    //이럴땐 (주소) var를 많이 씀.
     todolist.clear();
     var url = Uri.parse(
         "http://localhost:8080/Flutter/todolist_select_all.jsp?user_uId=$id");
-    //http 가 위주소를 다가져옴 //await <- 빌드가 끝날때까지 일단 기다려
+
     var response = await http.get(url);
 
-    /* 내가 알아보기 쉽게 변형해주기! */
     setState(() {
-      //화면 구성이 바뀌니까 setState 사용!
-      //body는 통째로 보여줘
-      //한글해주는건 utf8.decode(response.bodyBytes)로 해야함!
       var dataConvertedJSON = json.decode(utf8.decode(response.bodyBytes));
       List result = dataConvertedJSON['results']; //results 키값!
 
-      //print(result);
-      //result[0]['code'] = S001 <- 리스트에 넣어준거 불러오는법
-
-      //불러오고 수정한값을 이제 넣어주자!
       todolist.addAll(result);
-      //print(data[0]['code']); //flutter: S001
     });
 
-    //print(response.body);
     return true;
   }
 
